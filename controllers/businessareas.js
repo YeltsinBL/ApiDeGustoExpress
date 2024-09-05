02d4d471949e528d6a901cc4af43feb651a924de
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-09-05 15:08:11 -0500

    Creación del modelo

--- controllers/businessareas.js
+++ controllers/businessareas.js
@@ -1,9 +1,7 @@
+import {getAll} from '../models/businessareas.js'
 export const getItems = async(req, res)=>{
     try {
-        const listAll = [{'businessAreaId':1,'businessAreaName':'Restaurante'},
-            {'businessAreaId':2,'businessAreaName':'Hoteles'},
-            {'businessAreaId':3,'businessAreaName':'Cines'}
-        ]
+        const listAll = await getAll()
         res.send(listAll)
     } catch (e) {
         
---
+++ models/businessareas.js
@@ -0,0 +1,6 @@
+export const getAll = async () =>{
+    return [{'businessAreaId':1,'businessAreaName':'Restaurante'},
+        {'businessAreaId':2,'businessAreaName':'Hoteles'},
+        {'businessAreaId':3,'businessAreaName':'Cines'}
+    ]
+}
