a170e81fce5f37503d4c18b8d954a466442321ae
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-09-13 18:33:29 -0500

    Obtener la URL del root

--- utils.js
+++ utils.js
@@ -3,3 +3,8 @@
 import { createRequire } from 'node:module'
 const require = createRequire(import.meta.url) // obtenemos la dirección del archivo actual
 export const readJson = (path) => require(path)
+
+// Obtener la URL del root
+import * as url from "url";
+const directoryUrl = new URL(".", import.meta.url);
+export const directoryPath = url.fileURLToPath(directoryUrl);
