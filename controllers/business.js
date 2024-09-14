c1f0644c0a7d1641ef16f20f53f1ab585e9fddb8
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-09-13 22:35:27 -0500

    Cambiar ubicación de Multer: de Routers a Controller

--- controllers/business.js
+++ controllers/business.js
@@ -1,5 +1,68 @@
+import multer from "multer";
+import fs from 'fs'
+import path from "path";
+import { directoryPath } from '../utils.js'
 import { httpError } from "../helpers/handleError.js"
 import { getAll, getById,deleteById, create, upload } from '../models/business.js'
+
+// Función para verificar si el archivo es una imagen
+const imageFilter = (req, file, cb) => {
+    // Verificar la extensión del archivo
+    const filetypes = /jpeg|jpg|png/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+        return cb(null, true); // Acepta el archivo
+    } else {
+        return cb(new Error('Solo se permiten archivos de imagen')); // Rechaza el archivo
+    }
+};
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        if(!file) return
+        const dir = './images'.concat(`/${req.body.businessName.replace(/\s/g,'')}`, '/logo');
+        const storeFolder = directoryPath.concat(dir.substring(2))
+        if (!fs.existsSync(storeFolder)) {
+            fs.mkdirSync(storeFolder, { recursive: true });
+        }
+        cb(null, dir)
+    },
+    filename: function (req, file, cb) {
+        if(!file) return
+        const dir = './images'.concat(`/${req.body.businessName.replace(/\s/g,'')}`, '/logo');
+        // Verificar si ya existe algún archivo en la carpeta
+        fs.readdir(dir, (err, files) => {
+          if (err)return cb(err)
+          // Si hay archivos, eliminarlos
+          if (files.length > 0) {
+              files.forEach(existingFile => {
+                  const existingFilePath = path.join(dir, existingFile);
+                  fs.unlinkSync(existingFilePath); // Eliminar el archivo
+                  console.log(`Archivo existente ${existingFile} eliminado.`);
+              });
+          }
+          // Crear un nuevo nombre de archivo único o conservar el nombre original
+          const uniqueFilename = Date.now() + path.extname(file.originalname); // Nombre único para evitar colisiones
+          cb(null, uniqueFilename);  // Guardar el nuevo archivo con un nombre único
+        });
+    }
+  })
+
+const uploadImage = multer({ storage: storage, fileFilter: imageFilter })
+
+const uploadSingleImage = uploadImage.single('file')
+const uploadSingleImageAsync = (req, res) => {
+    return new Promise((resolve, reject) => {
+        uploadSingleImage(req, res, (err) => {
+            if (err) {
+                reject(err); // Rechaza la promesa si ocurre un error
+            } else {
+                resolve(req.file); // Resuelve la promesa con el archivo subido
+            }
+        });
+    });
+};
 export const getItems = async(req, res)=>{
     try {
         const listAll = await getAll()
@@ -35,11 +98,14 @@ export const deleteItemById = async(req, res) =>{
 }
 export const createItem = async(req, res) =>{
     try {
+        await uploadSingleImageAsync(req,res)
         req.body.businessLogo = req.file.path
         const result = await create({ input: req.body })
         res.status(201).json(result)
+        
     } catch (error) {
-        httpError(res,e)
+        return res.status(400).json({message: error.message})
+        // httpError(res,error)
     }
 }
 export const updateItem = async(req, res) =>{
--- routers/business.js
+++ routers/business.js
@@ -1,32 +1,12 @@
 import express from 'express'
-import multer from "multer";
-import fs from 'fs'
-import { directoryPath } from '../utils.js'
 import {getItems, getItemById, deleteItemById, createItem, updateItem} from '../controllers/business.js'
 
 const routers = express.Router()
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        if(!file) return
-        const dir = './images'.concat(`/${req.body.businessName.replace(/\s/g,'')}`, '/logo');
-        const storeFolder = directoryPath.concat(dir.substring(2))
-        if (!fs.existsSync(storeFolder)) {
-            fs.mkdirSync(storeFolder, { recursive: true });
-        }
-        cb(null, dir)
-    },
-    filename: function (req, file, cb) {
-        if(!file) return
-      cb(null, file.originalname)
-    }
-  })
-
-const upload = multer({ storage: storage })
 routers.get('/', getItems)
 routers.get('/:id', getItemById)
 routers.delete('/:id', deleteItemById)
-routers.post('/', upload.single('file'), createItem)
+routers.post('/', createItem)
 routers.put('/', updateItem)
 
 export default routers
